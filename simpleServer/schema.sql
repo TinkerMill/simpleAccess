2e7e50ea1a9a1b9e1bb100551a31a7cce61395d6
Ron Thomas <ronald.thomas@gmail.com>
2015-04-21 11:00:22 -0600

    Added users codes and extended userAccess table
    Added Matt and Ron's RFID codes and extended the userAccess table to include trainer, datecreated, and dateadded columns.

--- simpleServer/schema.sql
+++ simpleServer/schema.sql
@@ -3,16 +3,16 @@
 
 CREATE TABLE user(id INTEGER PRIMARY KEY, name TEXT, code TEXT);
 
-CREATE TABLE userAccess(id INTEGER PRIMARY KEY, user INTEGER, device INTEGER, level INTEGER);
+CREATE TABLE userAccess(id INTEGER PRIMARY KEY, user INTEGER, device INTEGER, level INTEGER, trainer INTEGER, datecreated TEXT, datemodified TEXT);
 
 CREATE TABLE device(id INTEGER PRIMARY KEY, name TEXT, description TEXT);
 
 insert into device (id, name, description) values (0, 'laser cutter', '80w laser cutter');
 insert into device (id, name, description) values (1, '3d printer', 'fancy 3d printer');
 
-insert into user (id, name, code) values (0, 'matt', 'abcde');
-insert into user (id, name, code) values (1, 'ron', 'abcdesdf');
+insert into user (id, name, code) values (0, 'matt', '04001D4868');
+insert into user (id, name, code) values (1, 'ron', '150060E726');
 
-insert into userAccess(id, user, device, level) values(0,0,0,1);
-insert into userAccess(id, user, device, level) values(1,0,1,1);
-insert into userAccess(id, user, device, level) values(2,1,0,2);
+insert into userAccess(id, user, device, level) values(0,0,0,1,1,datetime('now'),datetime('now'));
+insert into userAccess(id, user, device, level) values(1,0,1,2,1,datetime('now'),datetime('now'));
+insert into userAccess(id, user, device, level) values(2,1,0,1,1,datetime('now'),datetime('now'));
